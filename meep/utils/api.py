efd43f1c644f96cce80bd1b691d6d60bd0a57d99
Arturas Aleksandrauskas <artal938@student.liu.se>
2019-04-07 15:35:18 +0200

    asd

--- meep/utils/api.py
+++ meep/utils/api.py
@@ -7,7 +7,7 @@ def read(db):
             data_json = json.load(f)
             return data_json
     except OSError as err:
-        print(err)
+        #print(err)
         return None
 
 def write(db, data):
