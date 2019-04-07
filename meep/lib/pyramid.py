5eca6a00779b4a31e3bb5c13e338350e19e6700c
Arturas Aleksandrauskas <artal938@student.liu.se>
2019-04-07 15:27:43 +0200

    eval

--- meep/lib/pyramid.py
+++ meep/lib/pyramid.py
@@ -25,7 +25,7 @@ class Pyramid():
 		self.number_of_freqs = config["number_of_freqs"] 
 		self.cutoff = config["cutoff"]
 		self.debug = debug
-		self.source_direction = eval("mp.E"+(config["source_direction"]))
+		self.source_direction = eval(config["source_direction"])
 
 	def print(self,*args):
 		if self.debug:
