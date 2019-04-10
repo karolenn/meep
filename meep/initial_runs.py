fa08eb053f1bed6e20fd5c9ff2c2ead2f5e2b359
Arturas Aleksandrauskas <Arturas.a.junior@gmail.com>
2019-04-10 16:35:17 +0200

    addes setup

--- meep/initial_runs.py
+++ meep/initial_runs.py
@@ -8,10 +8,9 @@ def initial_runs(sim_name):
         print("could not open db/sim_spec/{}.json".format(sim_name))
         exit(0)
     
-    pyramid = Pyramid({'source_position': 0.06, 'pyramid_height': 2.2, 'pyramid_width': 2, 'source_direction': 'mp.Ey', 'frequency_center': 2, 'frequency_width': 0.5, 'number_of_freqs': 1, 'cutoff': 2}
-)
+    pyramid = Pyramid()
     for config in db:
-        pyramid = Pyramid(config["pyramid"])
+        pyramid.setup(config["pyramid"])
         print(config["pyramid"])
         result = pyramid.simulate(config["simulate"])
         data = sim_to_json(config, result)
