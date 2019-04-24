37f6386964840f684f673329f08f827d5ffce77c
arty <artal938@student.liu.se>
2019-04-24 07:49:00 +0200

    refactord simspec returns poits and a function to convert to json

--- meep/spec_generator.py
+++ meep/spec_generator.py
@@ -10,16 +10,12 @@ def singleLoop(db,conf,array):
     for n in range(len(array)):
         db[str(array)].append(array[n])
 
-def generate_eq_dist(x, y, z, template):
+def generate_eq_dist(x, y, z):
     tests = []
     for itx in linspace(x["from"], x["to"], x["steps"]):
         for ity in linspace(y["from"], y["to"], y["steps"]):
             for itz in linspace(z["from"], z["to"], z["steps"]):
-                tmp = copy.deepcopy(template)
-                tmp["pyramid"][z["name"]] = itz
-                tmp["pyramid"][x["name"]] = itx
-                tmp["pyramid"][y["name"]] = ity
-                tests.append(tmp)
+                tests.append((itx,ity,itz))
     return tests
 
 
@@ -51,101 +47,71 @@ def generate_rand_dist(limit_x, limit_y, limit_z, radius, satisfied, max_time = 
             selected.append((x, y, z))
     return selected
 
+def points_to_json(points, template):
+    tests = []
+    for x,y,z in points:
+        tmp = copy.deepcopy(template)
+        tmp["pyramid"]["pyramid_height"] = x
+        tmp["pyramid"]["pyramid_width"] = y
+        tmp["pyramid"]["source_position"] = z
+        tests.append(tmp)
+    return tests
 
 
-
-def test():
-    template={
-        "simulate": {
-            "resolution": 20,
-            "use_fixed_time": True,
-            "simulation_time": 1,
-            "dpml": 0.1,
-            "padding": 0.1,
-            "ff_pts": 1600,
-            "ff_cover": False,
-            "use_symmetries": True,
-            "calculate_flux": True,
-            "ff_calculations": True,
-            "ff_angle": 6,
-            "simulation_ratio": "6/5",
-            "substrate_ratio": "1/20"
-        },
-        "pyramid": {
-            "source_position": 0.06,
-            "pyramid_height": 3.1,
-            "pyramid_width": 2,
-            "source_direction": "mp.Ey",
-            "frequency_center": 2,
-            "frequency_width": 0.5,
-            "number_of_freqs": 1,
-            "cutoff": 2
-        },
-        "result": {}
-    }
-    x = {"name": "pyramid_height", "from": 1, "to":2 ,"steps": 5}
-    y = {"name": "pyramid_width", "from": 2, "to":4 ,"steps": 4}
-    z = {"name": "source_position", "from": 0.01, "to": 0.5 ,"steps": 4}
-    result = generate_eq_dist(x, y, z, template)
+def test(template):
+    x = {"from": 1, "to":2 ,"steps": 5}
+    y = {"from": 2, "to":4 ,"steps": 4}
+    z = {"from": 0.01, "to": 0.5 ,"steps": 4}
+    result = generate_eq_dist(x, y, z)
+    tests = points_to_json(result, template)
 
     #write("db/sim_spec/out.json", result)
-    #print(result)
-def testRand():
-    template={
-        "simulate": {
-            "resolution": 20,
-            "use_fixed_time": True,
-            "simulation_time": 1,
-            "dpml": 0.1,
-            "padding": 0.1,
-            "ff_pts": 1600,
-            "ff_cover": False,
-            "use_symmetries": True,
-            "calculate_flux": True,
-            "ff_calculations": True,
-            "ff_angle": 6,
-            "simulation_ratio": "6/5",
-            "substrate_ratio": "1/20"
-        },
-        "pyramid": {
-            "source_position": 0.06,
-            "pyramid_height": 3.1,
-            "pyramid_width": 2,
-            "source_direction": "mp.Ey",
-            "frequency_center": 2,
-            "frequency_width": 0.5,
-            "number_of_freqs": 1,
-            "cutoff": 2
-        },
-        "result": {}
-    }
+    print(tests)
+
+def testRand(template):
     
     x = {"from": 1, "to":5 }
     y = {"from": 1, "to":5 }
     z = {"from": 0.01, "to": 0.5 }
     result = generate_rand_dist(x, y, z, 0.1, 50)
-    x = []
-    y = []
-    z = []
-    tests = []
-    for a,b,c in result:
-        tmp = copy.deepcopy(template)
-        tmp["pyramid"]["pyramid_height"] = a
-        tmp["pyramid"]["pyramid_width"] = b
-        tmp["pyramid"]["source_position"] = c
-        tests.append(tmp)
-        x.append(a)
-        y.append(b)
-        z.append(c)
-    print(x)
-    print(y)
-    print(z)
+    x,y,z = zip(*result)
+
+    tests = points_to_json(result, template)
+
     ax = plt.axes(projection='3d')
     ax.scatter(x,y,z)
     plt.show()
     write("db/sim_spec/out.json", tests)
-    #print(result)
+    #print(tests)
 
 
-if __name__ == "__main__":  
-    testRand()
+if __name__ == "__main__":
+    template={
+        "simulate": {
+            "resolution": 20,
+            "use_fixed_time": True,
+            "simulation_time": 1,
+            "dpml": 0.1,
+            "padding": 0.1,
+            "ff_pts": 1600,
+            "ff_cover": False,
+            "use_symmetries": True,
+            "calculate_flux": True,
+            "ff_calculations": True,
+            "ff_angle": 6,
+            "simulation_ratio": "6/5",
+            "substrate_ratio": "1/20"
+        },
+        "pyramid": {
+            "source_position": 0.06,
+            "pyramid_height": 3.1,
+            "pyramid_width": 2,
+            "source_direction": "mp.Ey",
+            "frequency_center": 2,
+            "frequency_width": 0.5,
+            "number_of_freqs": 1,
+            "cutoff": 2
+        },
+        "result": {}
+    }  
+    test(template)
