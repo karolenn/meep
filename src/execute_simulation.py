006587e7403a2b9b1925d89594fee78612cc6a5c
Kalle spaceship <karljohan.olofsson@gmail.com>
2019-06-09 19:53:00 +0200

    execute sim to fields

--- src/execute_simulation.py
+++ src/execute_simulation.py
@@ -3,7 +3,7 @@ from utils.api import *
 
 def execute_simulation(config, output_file):
     write("db/tmp/tmp.json", config)
-    subprocess.run(['mpirun', "-np", "2", "python3", "lib/pyramid.py", output_file])
+    subprocess.run(['mpirun', "-np", "16", "python3", "lib/pyramid.py", output_file])
     
 
 
