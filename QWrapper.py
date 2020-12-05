I might have fixed C6 rotation of far field but needs some additional testing. Otherwise obtaining proper emission lobe might require more dipoles (50% likely). Also needs to fix integration method with spherical coordinates. Possibly with Jacobian determinant

QWrapper.py
@@ -1,5 +1,5 @@
 from functionality.api import read, db_to_array, polar_to_complex_conv, complex_to_polar_conv
-from functionality.functions import myPoyntingFlux,PolarizeInPlane, draw_uniform,rotate_list
+from functionality.functions import myPoyntingFlux,PolarizeInPlane, draw_uniform,rotate_list,rotate_list2
 from functionality.QWrapper_functions import *
 import matplotlib.pyplot as plt
 import numpy as np
@@ -15,13 +15,26 @@ def Qwell_wrapper(sim_name,number_of_dipoles):
     if db == None:
         print("could not open db/initial_results/{}.json".format(sim_name))
         exit(0)
-
     #load simulation parameters
     number_of_pyramids=len(db)
+
+    #Natural assumption is that sampling is according to the modified spherical sampling algorithm
     #add +1 for the (0,0,r) point
-    ff_pts = db[0]["simulate"]["ff_pts"]+1
-    range_npts = ff_pts
+    ff_pts = db[0]["simulate"]["ff_pts"]
+    npts = ff_pts
+    range_npts = int(ff_pts*ff_pts*2+1)
     theta = math.pi/db[0]["simulate"]["ff_angle"]
+    fibb_sampling = False
+    if fibb_sampling:
+            if theta == math.pi/6:
+                npts = ff_pts*3
+            else:
+                npts = ff_pts
+            range_npts=int((theta/math.pi)*npts) 
+
+
+    ff_pts = range_npts
+
     nfreq=db[0]["pyramid"]["number_of_freqs"]
 
     #array to save resulting E,H far-fields in
@@ -51,27 +64,25 @@ def Qwell_wrapper(sim_name,number_of_dipoles):
     total_flux_int = [0]*nfreq
     total_flux_int2 = []
 
-
     dipole_positions = []
-    dipole_positions_rotated = []
-
-    #
-    all_field_pos = []
 
     r=2*math.pow(db[0]["pyramid"]["pyramid_height"],2)*db[0]["pyramid"]["frequency_center"]*2*10
     S = 2*math.pi*pow(r,2)*(1-math.cos(theta))/range_npts
-    print('Initializing quantum well calculations, number of 3-group is:',number_of_pyramids/3)
+    print('Initializing quantum well calculations, number of 3-group is:',int(number_of_pyramids/3))
     print('r,theta,range_npts',r,theta,range_npts)
     print('S',S)
 
 
     #save the initial far-field coordinates. The assumption is now that all the 3-groups have the same far-field sampling coordinates!
     initial_ff_coords = []
-    far_field = db[0]["result"]["far_fields"]
-    for n in range(ff_pts):
+    far_field = db[5]["result"]["far_fields"]
+    print('ff',len(far_field))
+    for n in range(range_npts):
         ff_sampling_coord = far_field[n]["pos"]
         initial_ff_coords.append(ff_sampling_coord)
-
+    print('Number of sampling coordinates is:')
+    print(len(initial_ff_coords))
+    #print(initial_ff_coords)
     #far-field coordinates for all 3-groups after rotation
     poynting_field_coords = []
 
@@ -82,7 +93,6 @@ def Qwell_wrapper(sim_name,number_of_dipoles):
     poynting_total_field_rotated = [[0]*nfreq]*ff_pts
 
     #Initialize poynting field coordinates
-    poynting_total_field_rotated2 = initialize_poynting_far_field_rotated(ff_pts,nfreq)
     #Loop through each "3-group"
     for i in range(0,int(number_of_pyramids),3):
 
@@ -123,13 +133,14 @@ def Qwell_wrapper(sim_name,number_of_dipoles):
 
         #Calculate the resulting poynting field from the 3 group
         poynting_field_3g = calculate_poynting_field(summed_ff_3g,ff_pts,nfreq)
-        print('poynting 3g',poynting_field_3g)
         #add the poynting field from the 3-group to the total field
         poynting_total_field = add_poynting_fields(poynting_total_field,poynting_field_3g, ff_pts)
-        poynting_field_3g_rotated = rotate_list(poynting_field_3g,ff_pts,rotation_for_3g)
+
+        #Save a seperate poynting total field where the dipoles and the corresponding poynting field have rotated with n*60 degrees
+        poynting_field_3g_rotated = rotate_list2(poynting_field_3g,npts,rotation_for_3g)
+ 
         poynting_total_field_rotated = add_poynting_fields(poynting_total_field_rotated,poynting_field_3g_rotated, ff_pts)
 
-        #poynting_total_field_rotated2[rotation_for_3g] = add_poynting_fields(poynting_total_field_rotated2[rotation_for_3g],poynting_field_3g,ff_pts)
         #Add the poynting vector values 
 
         #ff_flux internal is set to 0 for each new three group. But the total quantum well energy values are added to the internal list each iteration
@@ -144,276 +155,65 @@ def Qwell_wrapper(sim_name,number_of_dipoles):
             total_flux[k] += db[i+0]["result"]["total_flux"][k]*wx_plane*wx_plane+db[i+1]["result"]["total_flux"][k]*wy_plane*wy_plane+db[i+2]["result"]["total_flux"][k]*wz_plane*wz_plane
             total_flux_internal[k] += total_flux[k]
 
-        #CALCULATE 3-group far field (ONLY FOR TESTING PURPOSES)
-        ff_flux_3group = [0]*nfreq
-        for n in range(ff_pts):
-            for k in range(nfreq):
-                ff_flux_3group[k] += S*poynting_field_3g[n][k]
-
-        #save the far-field coordinates
-
-        ff_coords = []
-
-
         ff_flux_int2.append(ff_flux_internal)
         total_flux_int2.append(total_flux_internal)
  
-
-        i_ = int(i/3)
         freq=2
-        current_total_ratio = round(ff_flux_int2[i_][freq]/total_flux_int2[i_][freq],4)
-
-        #print if the change from one iteration to the next changes by more than 20% 
-        if abs((current_total_ratio - ff_flux_int2[i_-1][freq]/total_flux_int2[i_ - 1][freq])/current_total_ratio) > 0.10:
-            print('huge diff detected:')
-            print('dipole wieghts',wx_plane,wy_plane,wz_plane)
-            print('index:',i_)
-            print('total flux internal:',total_flux_int2[i_][freq])
-            print('far field flux internal:',ff_flux_int2[i_][freq])
-            print('current total ratio:',current_total_ratio)
-            print('total flux internal prev:',total_flux_int2[i_-1][freq])
-            print('far field flux internal prev:',ff_flux_int2[i_-1][freq])
-            print('----')
-            print('this 3-groups total flux and far field (for a given freq)')
-            print('3g tot flux:',db[i+0]["result"]["total_flux"][freq]*wx_plane*wx_plane+db[i+1]["result"]["total_flux"][freq]*wy_plane*wy_plane+db[i+2]["result"]["total_flux"][freq]*wz_plane*wz_plane)
-            print('3g ff:',ff_flux_3group[freq])
-            print('LEE:',ff_flux_3group[freq]/(db[i+0]["result"]["total_flux"][freq]*wx_plane*wx_plane+db[i+1]["result"]["total_flux"][freq]*wy_plane*wy_plane+db[i+2]["result"]["total_flux"][freq]*wz_plane*wz_plane))
-      #  print(f"pyramid group {i_}, result: total flux: {round(total_flux_int2[i_][freq],4)}, far field: {round(ff_flux_int2[i_][freq],6)}, ratio: {current_total_ratio}")
 
     "calculate ff_flux for rotated poynting field"
     ff_flux_tot_rot = [0]*nfreq
     for n in range(ff_pts):
         for k in range(nfreq):
             ff_flux_tot_rot[k] += S*poynting_total_field_rotated[n][k]
-    #for n in range(ff_pts):
 
     print('ff flux tot rot:',ff_flux_tot_rot)
-    "TEST CODE FOR VERIFICATION"
-  #  print(all_field_pos)
-    print('ff db sum')
-    sum_ff=[0]*nfreq
-    sum_tot=[0]*nfreq
-    for pyramid in db:
-        for k in range(nfreq):
-            sum_ff[k] += pyramid["result"]["ff_at_angle"][k]
-            sum_tot[k] += pyramid["result"]["total_flux"][k]
-    print('db um',sum_ff,sum_tot)
 
     for n in range(ff_pts):
         for k in range(nfreq):
             ff_flux[k] += S*poynting_total_field[n][k]
 
-
     for k in range(nfreq):
         print('final freq',k,'ff_angle',round(ff_flux[k],4),'tot',round(total_flux[k],4),'ratio',round(100*ff_flux[k]/total_flux[k],2),'%')
 
-    #plot poynting coordinates
-    print('init ff card',initial_ff_coords)
-    x,y,z = create_far_field_coords(initial_ff_coords)
-    print(len(x))
-    ax2 = plt.axes(projection='3d')
-    ax2.scatter(x,y,z)
-    x = []
-    y = []
-    z = []
-    #unpack initial ff_coords
-    for k in range(len(initial_ff_coords)):
-        x.append(initial_ff_coords[k][0])
-        y.append(initial_ff_coords[k][1])
-        z.append(initial_ff_coords[k][2])
-    ax2.scatter(x,y,z)
-    plt.show()
+    #Plot LEE convergence
+    plot_LEE_convergence(ff_flux_int2,total_flux_int2)
 
-    ff_values = []
-    #print('rang',len(poynting_total_field_rotated2))
-    #print('field',poynting_total_field_rotated2)
-    #for i in range(6):
-    for n in range(ff_pts):
-        val = poynting_total_field_rotated[n][freq]
-        ff_values.append(val)
-    #print('ff al',ff_values)
-    #print('lenn',len(ff_values))
-    #plot emissi,on lobe
-    ax3 = plt.axes(projection='3d')
-    #ax3.plot_trisurf(x, y, ff_values,cmap='viridis', edgecolor='none')
-
-    print('XX',len(x))
-    print(ff_values)
-    X = []
-    Y = []
-    Z = []
-    for n in range(len(x)):
-        X.append(x[n]*ff_values[n])
-        Y.append(y[n]*ff_values[n])
-        Z.append(z[n]*ff_values[n])
-
-    ax3.plot_trisurf(X,Y,Z,cmap='jet',edgecolor='none')
-    
-    ax3.set_zlabel(r'$ flux')
-    ax3.set_title(r'$\lambda = 500 nm$')
-    ax3.set_xlabel('x-coordinates')
-    ax3.set_ylabel('y-coordinates')
-    plt.show()
-
-
-    ff_1 = []
-    ff_2 = []
-    ff_3 = []
-    tot_1 = []
-    tot_2 = []
-    tot_3 = []
-    ratio_1 = []
-    ratio_2 = []
-    ratio_3 = []
-    ratio_4 = []
-
-    ratio_5 = []
-    index = []
-    for n in range(len(ff_flux_int2)):
-        ratio_1.append(100*ff_flux_int2[n][0]/total_flux_int2[n][0])
-        ratio_2.append(100*ff_flux_int2[n][1]/total_flux_int2[n][1])
-        ratio_3.append(100*ff_flux_int2[n][2]/total_flux_int2[n][2])
-        ratio_4.append(100*ff_flux_int2[n][3]/total_flux_int2[n][3])
-        ratio_5.append(100*ff_flux_int2[n][4]/total_flux_int2[n][4])
-        index.append(n)
-
-
-    #plt.plot(index,ratio_1,color='red',label='714 nm')
-    plt.plot(index,ratio_2,color='orange',label='588 nm')#588 orange
-    plt.plot(index,ratio_3,color='g',label='500 nm')#500 g
-    plt.plot(index,ratio_4,color='b',label='435 nm')
-    #plt.plot(index,ratio_5,color='m',label=' 385 nm')
-    plt.legend(loc='best')
-    plt.grid()
-    plt.ylabel('LEE (%)')
-    plt.xlabel('Number of dipoles')
-    plt.show()
-
+    #Plot poynting coordinates
+    plot_poynting_coordinates(initial_ff_coords)
 
     pyramid_height = db[0]["pyramid"]["pyramid_height"]
     inner_pyramid_height = pyramid_height - 0.1
-    simulation_ratio = 6/5
-    subsrate_ratio = 1/10
-    sh = subsrate_ratio*pyramid_height
-    sz=pyramid_height*simulation_ratio
+    simulation_ratio = eval(db[0]["simulate"]["simulation_ratio"])
+    substrate_ratio = eval(db[0]["simulate"]["substrate_ratio"])
+   
 
     #PLOTTER FUNCTIONS
     #dipole plot
-    ax = plt.axes(projection='3d')
-    print('dipol pos list length',len(dipole_positions))
-    x_spos,y_spos,z_spos = zip(*dipole_positions)
-    X_ = []
-    Y_ = []
-    Z_ = []
-    xspos = []
-    yspos = []
-    zspos = []
-    for n in range(len(x_spos)):
-        #TODO: Create function that does this mapping
-        x = (inner_pyramid_height*z_spos[n]*math.cos(math.pi/6))/math.tan(62*math.pi/180)
-        y = y_spos[n]*math.tan(math.pi/6)*(inner_pyramid_height*z_spos[n]*math.cos(math.pi/6))/math.tan(62*math.pi/180)
-        z = sz/2-sh-inner_pyramid_height+inner_pyramid_height*z_spos[n]
-
-        #rotate dipole position
-        x,y,z = rotate_coordinate(x,y,z,rotation_integers[n])
-
-        xspos.append(x)
-        yspos.append(y)
-        zspos.append(z)
-
-    ax.scatter(xspos,yspos,zspos)
-    plt.show()
-
+    plot_dipoles_on_pyramids(dipole_positions,rotation_integers,pyramid_height,simulation_ratio,substrate_ratio)
 
     #field freq to plot
+    ff_pts_rot = []
+    for n in range(ff_pts):
+        ff_pts_rot.append(poynting_total_field_rotated[n][freq])
     ff_pts_norm = []
-    for i in range(int(number_of_pyramids/3)):
-        for n in range(ff_pts):
-            ff_pts_norm.append(poynting_total_field_rotated[i][n][freq])
-    max_val = max(ff_pts_norm)
+    for n in range(ff_pts):
+        ff_pts_norm.append(poynting_total_field[n][freq])
+    max_val2 = max(ff_pts_norm)
 
    # for n in range(ff_pts):
     #    ff_pts_norm[n] = ff_pts_norm[n]/max_val
     #print(ff_pts_norm)
 
-    ff_coords = []
-    #""   for k in range(len(all_ffields[0])):
-    #      ff_coords.append(all_ffields[0][k]["pos"])
-    print('len',len(poynting_field_coords))
-    print('len2',len(poynting_field_coords[1]))
-   # print(poynting_field_coords[1])
-    for i in range(int(number_of_pyramids/3)):
-        for n in range(ff_pts):
-            ff_coords.append(poynting_field_coords[i][n])
-
-    #print(ff_coords)
-    x_coord = []
-    phi = []
-    y_coord = []
-    theta = []
-  # print('ff_coords',ff_coords)
-   # print('ff',ff_pts_norm)
-    for k in range(len(ff_coords)):
-        x = ff_coords[k][0]
-        y = ff_coords[k][1]
-        if y == 0:
-            y = y + 1e-9
-        z = ff_coords[k][2]
-        x_coord.append(ff_coords[k][0])
-        y_coord.append(ff_coords[k][1])
-
-
-        phi_ = math.atan(x/y)
-        phi.append(phi_)
-        theta_ = math.atan(math.sqrt(x**2+y**2)/z)
-        theta.append(theta_)
-
-
-    ax.scatter(x_coord,y_coord,ff_pts_norm)
-    plt.show()
-    from scipy.interpolate import Rbf
-
-  #  RBF = Rbf(x_coord,y_coord,ff_pts_norm,epsilon = 2)
-
-    x = np.linspace(min(x_coord),max(x_coord))
-    y = np.linspace(min(y_coord),max(y_coord))
-
-
-    #generate a list of all possible poynting field cooordinates
-    
-
-   # rbf_data = RBF(x,y)
-    print('ff pts norm',len(ff_pts_norm))
-    print('stats')
-    import statistics as stats
-    print(stats.mean(ff_pts_norm))
-    print(stats.median(ff_pts_norm))
-    print(stats.stdev(ff_pts_norm))
-    #select cross sections
-    x_cr = []
-    y_cr = []
-    ff_pts_cr = []
-    for k in range(len(ff_coords)):
-        if abs(ff_coords[k][0]) < 20:
-            x_cr.append(ff_coords[k][0])
-            ff_pts_cr.append(ff_pts_norm[k])
-
- 
-    x_cr,ff_pts_cr = zip(*sorted(zip(x_cr,ff_pts_cr)))
-
-    print('xcord len',len(x_coord))
-    ax = plt.axes(projection='3d')
-    ax.plot_trisurf(x_coord, y_coord, ff_pts_norm,cmap='viridis', edgecolor='none')
-    ax.set_zlabel(r'$ flux')
-    ax.set_title(r'$\lambda = 500 nm$')
-    ax.set_xlabel('x-coordinates')
-    ax.set_ylabel('y-coordinates')
-  #  X, Y = np.meshgrid(x, y)
-   # z = RBF(X, Y)
-
-    plt.show()
+    plot_farfield(initial_ff_coords,ff_pts_rot)
+    plot_farfield(initial_ff_coords,ff_pts_norm)
+    #plt.show()
 
     plt.hist(ff_pts_norm,100)
+    #plt.show()
+    #ax4 = plt.axes(projection='3d')
+    #ax4.plot_trisurf(x,y, ff_pts_norm2,cmap='viridis', edgecolor='none')
+    #plt.show()
+    plt.hist(ff_pts_norm2,100,color='orange')
     plt.show()
    # plt.plot(x,z[24])
    # plt.xlabel('x-coordinates')